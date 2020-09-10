d01654afeb55ed6ac95d8ba2f808e1b9d04ba6a7
Inna Dyachkova <inna.dyachkova@gmail.com>
2020-09-10 08:26:54 +0200

    changed js and index.html

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
 var calc_button = document.getElementById("calc");
 calc_button.addEventListener("click", function(event) {
-    alert("Hello! I am an alert box!");
-})
+    var ones = parseInt(document.getElementById("player1_ones").value, 10);
+    var twos = parseInt(document.getElementById("player1_twos").value, 10);
+    var threes = parseInt(document.getElementById("player1_threes").value, 10);
+    var fours = parseInt(document.getElementById("player1_fours").value, 10);
+    var fives = parseInt(document.getElementById("player1_fives").value, 10);
+    var sixes = parseInt(document.getElementById("player1_sixes").value, 10);
+    var sum = ones + twos + threes + fours + fives + sixes;
+    document.getElementById("player1_sum").innerHTML = sum;
+    console.log(sum);
+    if (sum >= 63) {
+        document.getElementById("player1_bonus").innerHTML = 50;
+    } 
+}) 
